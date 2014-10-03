764b2d3d43d7352f9dd751064f888f7e8ce482e0
Alex Friberg <classay@gmail.com>
2014-10-02 23:47:49 -0400

    more style tweaks to form and password div

--- index.php
+++ index.php
@@ -9,14 +9,23 @@
 <body>
   <div class="title">So, you need a new password?</div>
   <div class="content">
-    <div class="section topLeft">So, you need a new password. You're in the right place, and with a little inspiration from <a target="_blank" href="http://xkcd.com/936/">the webcomic xkcd</a>, it will not only be strong, but you'll also remember it. To get there we need to ask you some questions:<br><br>
+    <div class="section topLeft">So, you need a new password. You're in the right place, and with a little inspiration from <a target="_blank" href="http://xkcd.com/936/">the webcomic xkcd</a>, it will not only be strong, but you'll also remember it. First, some questions:<br><br>
     
     <form action="index.php" method="GET">
-        <input id="wordCount" name="wordCount"type="number"  min="1" max="5" required><label for="wordCount"> How many words would you like your password to contain?</label><br><br>
+        <div class="question double">
+          <input id="wordCount" name="wordCount"type="number"  min="1" max="5" required>
+          <label for="wordCount">How many words would you like your new password to contain?</label>
+        </div>
         
-        <input type="checkbox" id="includeNumber" name="includeNumber" value="true"> Include a number?<br><br>
+        <div class="question">
+          <input type="checkbox" id="includeNumber" name="includeNumber" value="true">
+          <label for="includeNumber">Include a number?</label>
+        </div>
         
-        <input type="checkbox" id="includeSpecialChar" name="includeSpecialChar" value="true"> Include a special character?<br><br>
+        <div class="question">
+          <input type="checkbox" id="includeSpecialChar" name="includeSpecialChar" value="true">
+          <label for="includeSpecialChar">Include a special character?</label>
+        </div>
         
         <input id="submit" name="submit" type="submit" value="Make Password">
       </form>
